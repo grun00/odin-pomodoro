cf9880fe86bf97725157b8864cccea7c8c5f6993
grun00 <lucas.c.grunheidt@gmail.com>
2019-01-12 11:26:16 -0200

    finished layout

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-alert("!");
+function addTime(id, element){
+    if(id='rest-point'){
+        
+    }else{
+
+    }
+}
+
+function takeTime(){
+
+}
+
+const clockMinutes = document.querySelector("#minutes");
+const clockSeconds = document.querySelector("#seconds");
+const startPoint = document.querySelector('#start-point');
+const restPoint = document.querySelector('#rest-point');
