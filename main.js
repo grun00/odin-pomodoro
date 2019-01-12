e307bc4254f7699e796f60aef8c794b3367900c7
grun00 <lucas.c.grunheidt@gmail.com>
2019-01-12 14:15:37 -0200

    fixed timer

--- main.js
+++ main.js
@@ -1,21 +1,23 @@
+//0 padding for the clock
 Number.prototype.pad = function(size) {
     var s = String(this);
     while (s.length < (size || 2)) {s = "0" + s;}
     return s;
   }
-
+//functions to increase/decrease the time in the clocks
 function timeIncrease(element){
     element.textContent = (Number(element.textContent) + 1).pad(2);
 }
-
 function timeDecrease(element){
     element.textContent = (Number(element.textContent) - 1).pad(2);
 }
-
+//starts a countdown on the main clock
 function countDown(){
     if(mainMinutes.textContent == '-1'){
+        (flag > 0)? alert(flag) : alert(flag);
         mainMinutes.textContent = '00';
         mainSeconds.textContent = '00';
+        flag = (-1)*flag;
         clearInterval(pomodoroSession);
         clearInterval(restSession);
     }else if(mainSeconds.textContent == '00'){
@@ -25,8 +27,7 @@ function countDown(){
         timeDecrease(mainSeconds);}
 }
 
-const pomodoroSession = setInterval(countDown,1000)
-const restSession = setInterval(countDown,1000);
+
 //starting values for the clock
 const minuteTimer = document.querySelector('#minute-timer');
 const minuteRest = document.querySelector('#minute-rest');
@@ -43,6 +44,8 @@ const start = document.querySelector('#start');
 const stop = document.querySelector('#stop');
 const pause = document.querySelector('#pause');
 const reset = document.querySelector('#reset')
+//flag to indicate if it's session or rest timer
+let flag = 1;
 //arrow keys to increse starting minutes
 upTimer.addEventListener('click', function(){
     timeIncrease(minuteTimer);
@@ -63,5 +66,10 @@ downRest.addEventListener('click', function(){
 start.addEventListener('click', function(){
     mainSeconds.textContent = 59;
     timeDecrease(mainMinutes);
-    pomodoroSession;
+    if(flag > 0){
+        let pomodoroSession = setInterval(countDown, 1000);
+    }else {
+        mainMinutes.textContent = minuteRest.textContent;
+        let restSession = setInterval(countDown,1000);
+    }
 })
